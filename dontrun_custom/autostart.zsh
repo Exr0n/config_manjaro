782c651e995cb5744f8c55fc00a46f125101d3a3
Exr0n <mail@exr0n.com>
2021-05-23 10:06:26 -0700

    assign autostart + discord workspaces

--- dontrun_custom/autostart.zsh
+++ dontrun_custom/autostart.zsh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 source $XDG_CONFIG_HOME/dontrun_custom/env.zsh
 
-to_launch=( $HOME/projects/corsaurus )
+to_launch=( $HOME/projects/corsaurus $HOME/projects/Taproot )
 
 for projpath in $to_launch; do
-    alacritty --hold --working-directory $projpath -e ./start.sh &
+    alacritty\
+        --hold\
+        --class "$(basename $projpath),drc_autostart"\
+        --title "drc_autostart $(basename $projpath)"\
+        --working-directory $projpath\
+        -e ./start.sh &
 done

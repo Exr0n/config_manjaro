6109796892024a0f3e005302b41a795b471aa52b
Exr0n <mail@exr0n.com>
2021-06-19 15:13:37 -0700

    autostart delete alacritty + website

--- dontrun_custom/autostart.zsh
+++ dontrun_custom/autostart.zsh
@@ -1,15 +1,9 @@
 #!/bin/zsh
 source $XDG_CONFIG_HOME/dontrun_custom/env.zsh
 
-to_launch=( $HOME/projects/corsaurus $HOME/projects/Taproot )
+to_launch=( $HOME/projects/corsaurus $HOME/projects/Taproot $HOME/website )
 
 for projpath in $to_launch; do
-    #alacritty\
-    #    --hold\
-    #    --class "$(basename $projpath),drc_autostart"\
-    #    --title "drc_autostart $(basename $projpath)"\
-    #    --working-directory $projpath\
-    #    -e ./start.sh &
     kitty\
         -1\
         --hold\

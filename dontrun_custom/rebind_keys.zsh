37c25c968f866bf005e56f672b36c0679d3fc569
Exr0n <mail@exr0n.com>
2021-07-14 10:56:02 -0700

    interception-tools + dual-function-keys on new keyboard

--- dontrun_custom/rebind_keys.zsh
+++ dontrun_custom/rebind_keys.zsh
@@ -3,7 +3,7 @@ xbindkeys
 
 # keyboard
 xset r rate 170 40
-setxkbmap -option caps:ctrl_modifier
+#setxkbmap -option caps:ctrl_modifier
 #xmodmap -e 'keycode 36='    # disable return key, use Alt_R instead
 #xmodmap -e 'keycode 36=Return'    # put it back
 #xmodmap -e 'keycode ='
@@ -11,7 +11,8 @@ setxkbmap -option caps:ctrl_modifier
 killall xcape # reset keybindings
 #xcape -t 200 -e 'Caps_Lock=Escape;Shift_L=bracketleft;Shift_R=bracketright;Alt_L=Shift_L|braceleft;Alt_R=Shift_L|braceright'
 #xcape -t 200 -e 'Caps_Lock=Escape;Shift_L=bracketleft;Shift_R=bracketright;Alt_L=Shift_L|braceleft'
-xcape -t 200 -e 'Caps_Lock=Escape;Alt_R=Return'
+#xcape -t 200 -e 'Caps_Lock=Escape;Alt_R=Return'
+# all this stuff was moved to interception tools
 
 # mouse
 xinput set-prop 'pointer:Razer Razer DeathAdder 2013' 'libinput Natural Scrolling Enabled' 1
